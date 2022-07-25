8e92fad3d028b1e6751b011962002f90872d78b6
progLime <93077970+progLime@users.noreply.github.com>
2022-07-25 18:53:17 +0300

    leaflet initialization

--- client/src/Pages/User/HomePage.js
+++ client/src/Pages/User/HomePage.js
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/core";
 
 import { useLocation } from "react-router-dom";
+import { Icon } from 'leaflet';
+import { MapContainer, Popup, Marker, TileLayer } from "react-leaflet"
 
 import Constants from "../../Shared/Constants";
 
@@ -13,6 +15,9 @@ const useStyles = makeStyles({
   }
 });
 
+
+
+
 const HomePage = ({ UserId }) => {
   // Material UI Styles
   const classes = useStyles();
@@ -21,9 +26,33 @@ const HomePage = ({ UserId }) => {
 
   const { userData } = location.state;
 
+
+  let currentDateAndTime = new Date();
+
+  let currentDay = currentDateAndTime.getDay()
+
+  let currentHour = currentDateAndTime.getHours();
+  
+
+
   return (
     <div className={classes.homePageContainer}>
       Home
+
+      <div className='leaflet-container'>
+        <MapContainer center={[38.246639, 21.734573]} zoom={12} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+          />
+          <Marker position={[38.246639, 21.734573]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>   
+         </MapContainer>
+      </div>
+
     </div>
   );
 };
